a2fca3cd59a3dcda008ca77450cbf54a1a71dae6
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-09-12 00:07:45 +0000

    Brian K-Homework 2-multiply_numbers

--- run_tests.py
+++ run_tests.py
@@ -1,18 +1,11 @@
-#from ast import Suite
-#from typing import Self
 import unittest
 '''
 the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
+from tests.homework.b_in_proc_out import tests_in_proc_out
 #from tests.examples.a_example import tests_devprocess
 #from tests.homework.c_decisions import tests_decisions
 
-
-from tests.examples.c_decisions import tests_decisions
-
-#suite = unittest.TestLoader().loadTestsFromModule(tests_devprocess)
-#unittest.TextTestRunner(verbosity=2).run(Suite)
-
-suite = unittest.TestLoader().loadTestsFromModule(tests_decisions)
+suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
 unittest.TextTestRunner(verbosity=2).run(suite)
