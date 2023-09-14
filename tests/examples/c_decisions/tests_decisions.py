7e7967c853f13345e686ce496f9151b650085dcc
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-09-14 00:04:01 +0000

    Lecture Example Get Letter Grade

--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -1,7 +1,8 @@
 import unittest
+#from homework.c_decisions.decisions import get_letter_grade
 
 #from decisions import is_consonant, is_odd, is_vowel
-from src.examples.c_decisions.decisions import get_and_result, get_or_result, is_consonant, is_even, is_odd, is_overtime, is_vowel, test_config, get_notted_value
+from src.examples.c_decisions.decisions import get_and_result, get_letter_grade, get_or_result, is_consonant, is_even, is_odd, is_overtime, is_vowel, test_config, get_notted_value
 
 class Test_Config(unittest.TestCase):
 
@@ -45,3 +46,14 @@ class Test_Config(unittest.TestCase):
     def test_is_overtime(self):
         self.assertEqual(False, is_overtime(30))
         self.assertEqual(True, is_overtime(41))
+
+    def test_get_letter_grade(self):
+        self.assertEqual(get_letter_grade(105), "Invalid Grade")
+        self.assertEqual(get_letter_grade(100), "A")
+        self.assertEqual(get_letter_grade(95), "A")
+        self.assertEqual(get_letter_grade(90), "A")
+        self.assertEqual(get_letter_grade(85), "B")
+        self.assertEqual(get_letter_grade(75), "C")
+        self.assertEqual(get_letter_grade(65), "D")
+        self.assertEqual(get_letter_grade(55), "F")
+        self.assertEqual(get_letter_grade(-5), "Invalid Grade")
