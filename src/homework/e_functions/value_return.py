7e046956bbee8b4002ba382dfa290af65fb90b25
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-09-26 03:48:47 +0000

    lecture examples and hw 4 shell

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,9 @@
 #
+#takes an epoch_seconds parameter
+def get_hour(): 
+
+    #takes an epoch_seconds parameter
+def get_minutes(): 
+
+    #takes an epoch_seconds parameter
+def get_seconds():
--- tests/homework/e_functions/tests_functions.py
+++ tests/homework/e_functions/tests_functions.py
@@ -1,1 +1,21 @@
 #
+import unittest
+
+from src.homework.e_functions.value_return import get_hour
+from src.homework.e_functions.value_return import get_minutes
+from src.homework.e_functions.value_return import get_seconds
+
+
+class Test_Config(unittest.TestCase):
+
+#Test get hour
+#Test that get_hour with parameter value 3800 returns the value 1.
+#Test that get_hour with parameter value 3600 returns the value 1.
+
+#Test get minutes
+#Test that get_minutes with parameter value 3800 returns the value 3.
+#Test that get_minutes with parameter value 3600 returns the value 0.
+
+#Test case function test_get_seconds
+#Test that get_seconds with parameter value 3800 returns the value 20.
+#Test that get_seconds with parameter value 3600 returns the value 0.
