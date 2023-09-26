c69aebde8f39abdedd00747565a1042f60157f11
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-09-26 00:05:47 +0000

    nested while and multiplication tables

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -7,9 +7,11 @@ import repetition
 
 #result = repetition.for_sum_of_squares(5)
 #print("sum of squares is: " + str(result))
-#repetition.for_num_range_w_start_value(1, 30)
-#repetition.for_num_range_w_step_value(1, 20, 2)
-#repetition.for_num_range_w_start_value(26, 0, -2)
-#repetition.for_display_squares(0, 5)0
+#repetition.for_num_range_w_start_value(5, 10)
+#repetition.for_num_range_w_step_value(26, 0, -3)
+#repetition.for_display_squares(0, 5)
 #repetition.while_validate_user_input()
-repetition.nested_while_loop(3, 3)
+#repetition.nested_while_loop(3, 3)
+#repetition.nested_for_loop(3, 3)
+#repetition.for_multiplication_table(10, 10)
+repetition.while_multiplication_table(10, 10)
