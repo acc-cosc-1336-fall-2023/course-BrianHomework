2e4e2008ad5edd6c9dd65b738ce50e3ae4461481
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-10-05 05:50:03 +0000

    built in string functions

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,5 +1,4 @@
 import unittest
-
 from src.examples.h_strings.strings import concat_strings, search_in_string, slice_string, slice_w_step_value, test_config
 
 class Test_Config(unittest.TestCase):
@@ -40,3 +39,44 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(search_in_string(str1, str2), True)
         self.assertEqual(search_in_string("efg", str2), False)
 
+    def test_is_string_digit(self):
+        str1 = "1200"
+        self.assertEqual(str1.isdigit(), True)
+        self.assertEqual("ab".isdigit(), False)
+
+    def test_string_is_alpha(self):
+        str1 = "abcdefg"
+        self.assertEqual(str1.isalpha(), True)
+        self.assertEqual("$%1".isalpha(), False)
+
+    def test_string_is_upper(self):
+        str1 = "XYZ"
+        self.assertEqual(str1.isupper(), True)
+
+    def test_string_to_lower(self):
+        str1 = "aBcDeF"
+        self.assertEqual(str1.lower(), "abcdef")
+
+    def test_string_to_upper(self):
+        str1 = "aBcDeF"
+        self.assertEqual(str1.upper(), "ABCDEF")
+
+    def test_strip_char_from_strings(self):
+        str1 = "eeabcdefegefee"
+        self.assertEqual(str1.strip("e"), "abcdefegef") #Will remove from e from beginning and end
+
+    def test_lstrip_char_from_strings(self):
+        str1 = "aaaaxyz"
+        self.assertEqual(str1.lstrip("a"), "xyz")
+
+    def test_rstrip_char_from_strings(self):
+        str1 = "abcyyy"
+        self.assertEqual(str1.rstrip("y"), "abc")
+
+    def test_replace_from_string(self):
+        str1 = "eeabcdefegefe"
+        self.assertEqual(str1.replace("e", ""), "abcdfgf")
+
+    
+
+    
