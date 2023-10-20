249d871f89d26e42a751be5511b283045e3ae52b
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-10-20 03:39:02 +0000

    HomeWork 6 Was Rough

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,76 @@
-#
+from dictionary import get_p_distance_matrix, get_p_distance
+#import dictionary
+
+def is_valid_sequence(sequence):
+    valid_chars = {'A', 'C', 'G', 'T', 'a', 'c', 'g', 't'}
+    return all(char in valid_chars for char in sequence)
+
+def get_valid_sequence_count():
+    while True:
+        n = input("Enter the number of sequences: ")
+        if not n.isdigit():
+            print("Invalid input. Please enter a valid number for the number of sequences.")
+        else:
+            return int(n)
+
+
+while True:
+    print("\nMenu:")
+    print("1- Get p distance matrix")
+    print("2- Exit")
+    choice = input("Select an option: ")
+
+    if choice == '1':
+        n = get_valid_sequence_count()
+        sequences = []
+        length = None  # Initialize the length variable
+
+        for i in range(n):
+            sequence = input(f"Enter sequence {i + 1}: ").strip()
+            if length is None:
+                length = len(sequence)  # Initialize the length with the first sequence's length
+            elif len(sequence) != length:
+                print("The sequences you entered are not the same length. Please try again.")
+                break
+            elif not is_valid_sequence(sequence):
+                print("Invalid characters in the sequence. Please use only 'A', 'C', 'G', or 'T' (case-insensitive).")
+                break
+            sequences.append(list(sequence))
+        else:
+            distance_matrix = get_p_distance_matrix(sequences)
+            print("P-distance matrix:")
+            for row in distance_matrix:
+                print(' '.join(f'{distance:.5f}' for distance in row))
+    elif choice == '2':
+        print("Exiting the program.")
+        break
+    else:
+        print("Invalid option. Please choose 1 or 2.")
+
+
+
+'''''
+while True:
+        print("\nMenu:")
+        print("1- Get p distance matrix")
+        print("2- Exit")
+        choice = input("Select an option: ")
+
+        if choice == '1':
+            sequences = []
+            n = int(input("Enter the number of sequences: "))
+            for i in range(n):
+                sequence = input(f"Enter sequence {i + 1}: ").strip()
+                sequences.append(list(sequence))
+            
+            distance_matrix = get_p_distance_matrix(sequences)
+            print("P-distance matrix:")
+            for row in distance_matrix:
+                print(' '.join(f'{distance:.5f}' for distance in row))
+        elif choice == '2':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid option. Please choose 1 or 2.")
+'''''
+    
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,32 @@
 #
+import unittest
+
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_p_distance(self):
+        list1 = ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']
+        list2 = ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C']
+        result = get_p_distance(list1, list2)
+        self.assertEqual(result, 0.4)
+
+    def test_get_p_distance_matrix(self):
+        sequences = [
+            ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A'],
+            ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C'],
+            ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'T'],
+            ['G', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']
+        ]
+        expected_matrix = [
+            [0.0, 0.4, 0.1, 0.1],
+            [0.4, 0.0, 0.4, 0.3],
+            [0.1, 0.4, 0.0, 0.2],
+            [0.1, 0.3, 0.2, 0.0]
+        ]
+        result = get_p_distance_matrix(sequences)
+
+        for i in range(len(sequences)):
+            for j in range(len(sequences)):
+                self.assertEqual(result[i][j], expected_matrix[i][j])
