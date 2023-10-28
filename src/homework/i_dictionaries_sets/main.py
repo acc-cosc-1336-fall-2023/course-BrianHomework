c1f0d4e15888f908dd9a4c75a0c9bb9eea0e1594
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-10-28 04:33:04 +0000

    HW 7

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -5,36 +5,129 @@ def display_menu():
     print("1- Add or Update Item")
     print("2- Delete Item")
     print("3- Exit")
+    print("4- Check Inventory")
+
+def main():
+    inventory = {}  # Initialize an empty inventory dictionary
+
+    while True:
+        display_menu()
+        choice = input("Select an option (1, 2, 3, or 4): ")
+
+        if choice == '1':
+            while True:
+                # Add or update item
+                item_name = input("Enter the item name: ")
+                try:
+                    quantity = int(input("Enter the quantity: "))
+                    add_inventory(inventory, item_name, quantity)
+                    print(f"{item_name} with a quantity of {quantity} has been added or updated.")
+                except ValueError:
+                    print("Invalid quantity. Please enter a valid number.")
+                another_item = input("Would you like to add another item to inventory (Y/N)? ").strip().lower()
+                if another_item not in ['y', 'yes']:
+                    break
+
+        elif choice == '2':
+            # Delete item or quantity
+            while True:  # Add a loop to handle multiple deletions
+                item_name = input("Enter the item name to delete: ")
+                result = remove_inventory_widget(inventory, item_name)
+                print(result)
+
+                another_deletion = input("Do you want to delete another item (Y/N)?: ").strip().lower()
+                if another_deletion not in ['y', 'yes']:
+                    break  # Break out of the deletion loop if not 'Y' or 'yes    
+
+        elif choice == '3':
+            # Exit the program
+            print("Exiting the program.")
+            break
+
+        elif choice == '4':
+            if not inventory:  # Check if inventory is empty
+                print("No Inventory to Count. Add Items if you have any.")
+
+            else:
+                for item_name, quantity in inventory.items():
+                    print(f"{item_name}: {quantity}")
+
+                menu = input("Press Enter to go back to the main menu")
+                if menu != None:
+                    print("Going to the main menu")
+
+        else:
+            # Invalid input
+            print("Select a Valid Input, please try again.")
+main()
+
+
+
+
+
+'''
+def display_menu():
+    print("Inventory Menu\n")
+    print("1- Add or Update Item")
+    print("2- Delete Item")
+    print("3- Exit")
+    print("4- Check Inventory")
 
 def main():
         inventory = {}  # Initialize an empty inventory dictionary
 
         while True:
             display_menu()
-            choice = input("Select an option (1, 2, or 3): ")
+            choice = input("Select an option (1, 2, 3, or 4): ")
 
             if choice == '1':
-                # Add or update item
-                item_name = str(input("Enter the item name: "))
-                try:
-                    quantity = int(input("Enter the quantity: "))
-                    add_inventory(inventory, item_name, quantity)
-                    print(f"{item_name} with a quantity of {quantity} has been added or updated.")
-                except ValueError:
-                     print("That aint a number!")
-                           
+                while True:
+                    # Add or update item
+                    item_name = str(input("Enter the item name: "))
+                    try:
+                        quantity = int(input("Enter the quantity: "))
+                        add_inventory(inventory, item_name, quantity)
+                        print(f"{item_name} with a quantity of {quantity} has been added or updated.")
+                    except ValueError:
+                        print("That aint a number!")
+                    while True:
+                        another_item = input("Would you like to add another item to inventory Y/N?: ").strip().lower()
+                        if another_item in ['y', 'yes']:
+                            break
+                        if another_item in ['n', 'no']:
+                            break
+                    if another_item in ['n', 'no']:
+                            break
+
+
             elif choice == '2':
                 # Delete item
-                item_name = input("Enter the item name to delete: ")
-                result = remove_inventory_widget(inventory, item_name)
-                print(result)
+                while True: #Add loop to handle multi deletions
+                    item_name = str(input("Enter the item name to delete: "))
+                    result = remove_inventory_widget(inventory, item_name)
+                    print(result)
+
+                    another_deletion = input("Do you want to delete another item (Y/N)?: ").strip().lower()
+                    if another_deletion not in ['y', 'yes']:
+                        break  # Break out of the deletion loop if not 'Y' or 'yes
 
             elif choice == '3':
                 # Exit the program
                 print("Exiting the program.")
                 break
 
+            elif choice == '4':
+                for item_name in inventory.items():
+                      print(item_name)
+
+                menu = None
+                menu = input("press enter to go to the main menu")
+                if menu != None:
+                    print("going to main menu")
+
             else:
             # Invalid input
                 print("Select a Valid Input, please try again.")
-    
+
+main()
+'''    
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,31 +1,34 @@
 #
 import unittest
 
-from src.homework.i_dictionaries_sets.dictionary import get_p_distance, get_p_distance_matrix
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
 
 class Test_Config(unittest.TestCase):
 
-    def test_get_p_distance(self):
-        list1 = ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']
-        list2 = ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C']
-        result = get_p_distance(list1, list2)
-        self.assertEqual(result, 0.4)
+    def test_add_inventory(self):
+        inventory = {}  # Initialize an empty inventory dictionary
 
-    def test_get_p_distance_matrix(self):
-        sequences = [
-            ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A'],
-            ['G', 'A', 'T', 'T', 'C', 'A', 'T', 'T', 'T', 'C'],
-            ['T', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'T'],
-            ['G', 'T', 'T', 'C', 'C', 'A', 'T', 'T', 'T', 'A']
-        ]
-        expected_matrix = [
-            [0.0, 0.4, 0.1, 0.1],
-            [0.4, 0.0, 0.4, 0.3],
-            [0.1, 0.4, 0.0, 0.2],
-            [0.1, 0.3, 0.2, 0.0]
-        ]
-        result = get_p_distance_matrix(sequences)
+        # Adding Widget1 with quantity of 10
+        add_inventory(inventory, 'Widget1', 10)
+        self.assertEqual(inventory['Widget1'], 10)
 
-        for i in range(len(sequences)):
-            for j in range(len(sequences)):
-                self.assertEqual(result[i][j], expected_matrix[i][j])
+        # Updating the existing Widget1 with quantity of 25
+        add_inventory(inventory, 'Widget1', 25)
+        self.assertEqual(inventory['Widget1'], 35)
+
+        # Updating the existing Widget1 with a negative quantity
+        add_inventory(inventory, 'Widget1', -10)
+        self.assertEqual(inventory['Widget1'], 25)
+
+    def test_remove_inventory_widget(self):
+        inventory = {}  # Initialize an empty inventory dictionary
+
+        # Adding two widgets with quantities of your choice
+        add_inventory(inventory, 'Widget1', 10)
+        add_inventory(inventory, 'Widget2', 15)
+
+        # Removing Widget1
+        result = remove_inventory_widget(inventory, 'Widget1')
+        self.assertEqual(result, 'Record deleted')
+        self.assertEqual(len(inventory), 1)  # Length of dictionary should be 1
+        self.assertIn('Widget2', inventory)  # Widget2 should still exist
