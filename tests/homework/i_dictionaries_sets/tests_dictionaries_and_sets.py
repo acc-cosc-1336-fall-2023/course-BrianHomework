bf77efcf51fc7d8ffea7a7f465dc70839b1fb404
BrianHomework <143046531+BrianHomework@users.noreply.github.com>
2023-11-03 01:34:20 +0000

    HW 8 Just Tests?

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,4 +1,54 @@
 #
+
+#sets
+baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+
+#Finding the intersection of the sets
+intersection = baseball.intersection(basketball)
+print("Students who play both sports:", intersection)  
+
+#Finding the union of the sets
+union = baseball.union(basketball)
+print("Students who play either sport:", union)  
+
+#Finding the difference of baseball and basketball sets
+difference_baseball = baseball.difference(basketball)
+print("Students who play baseball but not basketball:", difference_baseball)  
+
+#Finding the difference of basketball and baseball sets
+difference_basketball = basketball.difference(baseball)
+print("Students who play basketball but not baseball:", difference_basketball)  
+#Finding the difference of baseball and basketball sets (baseball - basketball)
+difference_baseball = baseball.difference(basketball)
+print("Students who play baseball but not basketball:", difference_baseball)
+
+#Finding the symmetric difference of the sets
+symmetric_difference = baseball.symmetric_difference(basketball)
+print("Students who play one sport but not both:", symmetric_difference)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''''
 import unittest
 
 from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
@@ -32,3 +82,4 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(result, 'Record deleted')
         self.assertEqual(len(inventory), 1)  # Length of dictionary should be 1
         self.assertIn('Widget2', inventory)  # Widget2 should still exist
+'''        
